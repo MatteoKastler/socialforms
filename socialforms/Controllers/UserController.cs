8c3ab7828f29eab94d34d0960331ce0928ca884f
matteo <deinemutter@hathaarigeeier.at>
2021-11-24 13:51:02 +0100

    ccs changes usw

--- socialforms/Controllers/UserController.cs
+++ socialforms/Controllers/UserController.cs
@@ -4,19 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace socialforms.Controllers
-{
-    public class UserController
-    {
-
-    }
-
-    public IActionResult Login()
-    {
-        return View();
-    }
-    public IActionResult Registration()
-    {
-        return View();
+namespace socialforms.Controllers {
+    public class UserController : Controller { 
+        public IActionResult Login() {
+            return View();
+        }
+        public IActionResult Registration() {
+            return View();
+        }
     }
 }
