4efe45378f00beb519f9951834549f9822483bed
Paula Lechner <paula.lechner.tirol@gmail.com>
2021-11-24 13:56:29 +0100

    stuff

---
+++ socialforms/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace socialforms.Models
+{
+    public class User
+    {
+
+        private int _personId;
+
+        public int PersonId
+        {
+            get { return this._personId; }
+            set
+            {
+                if (value >= 0)
+                {
+                    this._personId = value;
+                }
+            }
+        }
+
+        public string Username { get; set; }
+
+        public String Password { get; set; }
+        public DateTime Birthdate { get; set; }
+
+
+    }
+}
