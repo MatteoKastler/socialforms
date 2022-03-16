1904ba54b3313028ba753e836e9083bc86a8b5d8
Paula Lechner <paula.lechner.tirol@gmail.com>
2022-03-16 13:33:03 +0100

    1234

--- socialforms/Models/DB/IRepositoryQuestions.cs
+++ socialforms/Models/DB/IRepositoryQuestions.cs
@@ -16,8 +16,8 @@ namespace socialforms.Models.DB
         bool Delete(int qstId);
         User GetQuestion(int qstId);
         List<Question> GetAllQuestions();
+        int cntQuestions(int formId);
         bool Update(int qstId, Question NewQuestionData);
-
         int cntAnswers(int qstId);
         //weitere Methoden
     }
