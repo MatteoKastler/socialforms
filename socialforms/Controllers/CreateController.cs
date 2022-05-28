c509efa6a42fbff73f1874536bcb3bfdacdabaf6
Paula Lechner <paula.lechner.tirol@gmail.com>
2022-05-28 07:44:08 +0200

    some adjustments

--- socialforms/Controllers/CreateController.cs
+++ socialforms/Controllers/CreateController.cs
@@ -4,9 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using socialforms.Models;
+using System.Data.Common;
+using socialforms.Models.DB.sql;
 
 namespace socialforms.Controllers {
     public class CreateController : Controller {
+
+        private FormQuery _rep = new FormQuery();
         public IActionResult Index() {
             return View();
         }
@@ -17,6 +21,58 @@ namespace socialforms.Controllers {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Createform(Form qstData)
+        {
+            if (qstData == null)
+            {
+                return RedirectToAction("Createform");
+            }
+
+            ValidateRegistrationData(qstData);
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _rep.Connect();
+                    if (_rep.Insert(qstData))
+                    {
+                        return View("_Message", new Message("Creation", "Ihr socialform wurde erfolgreich abgespeichert"));
+                    }
+                    else
+                    {
+                        return View("_Message", new Message("Creation", "Bitte versuchen Sie es später erneut"));
+                    }
+
+                }
+                catch (DbException)
+                {
+                    return View("_Message", new Message("Creation", "Datenbankfehler", "Bitte versuchen Sie es später erneut"));
+                }
+                finally
+                {
+                    _rep.Disconnect();
+                }
+
+
+            }
+
+            return View(qstData);
+        }
+
+        private void ValidateRegistrationData(Form f)
+        {
+            if (f == null)
+            {
+                return;
+            }
+        }
+
     }
 
-}
+    
+
+
+ }
