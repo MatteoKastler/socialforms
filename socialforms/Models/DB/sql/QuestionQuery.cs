63df07ed8bb0eff35fbd8b4317434fbcd290a5ff
matteo <deinemutter@hathaarigeeier.at>
2022-06-07 18:57:35 +0200

    bla

--- socialforms/Models/DB/sql/QuestionQuery.cs
+++ socialforms/Models/DB/sql/QuestionQuery.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace socialforms.Models.DB.sql {
     public class QuestionQuery : IQuestionQuery {
-        private string _connString = "Server=localhost;Port=3306;Database=socialforms;uid=root;pwd=MBigubb75#";
+        private string _connString = "Server=localhost;Port=3308;Database=socialforms;uid=root;pwd=toor";
         DbConnection _conn;
 
         public void Connect() {
