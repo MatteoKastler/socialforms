3697d4f9986849f4a020137e15c281ff62627ec5
Paula Lechner <paula.lechner.tirol@gmail.com>
2022-06-15 08:57:00 +0200

    jquery eingebaut

---
+++ socialforms/wwwroot/js/registration.js
@@ -0,0 +1,25 @@
+﻿/*alert("registration wurde eingebunden")*/
+
+$(document).ready(() => {
+    $("#Username").blur(() => {
+        let name = $("#Username").val();
+        $.ajax({
+            url: "/user/checkUsername",
+            method: "GET",
+            data: { username: $("#Username").val() }
+        })
+            .done((dataFromServer) => {
+                if (dataFromServer == true) {
+                    $("#nameMessage").css("visibility", "visible");
+                    $("#Username").addClass("redBorder");
+                } else {
+                    $("#nameMessage").css("visibility", "hidden");
+                    $("#Username").removeClass("redBorder");
+                }
+            })
+            .fail(() => {
+                alert("Server/URL nicht erreichbar")
+            });
+    });
+
+});
